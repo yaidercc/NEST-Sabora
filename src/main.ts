3f48b30df36a25497ac985b66a1d112f71da1f3b
yaider <yaiderc19@gmail.com>
2025-09-17 15:30:28 -0500

    feat: connecting with render port

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   logger.log(`Server started on the port ${process.env.PORT}`)
 }
 bootstrap();
